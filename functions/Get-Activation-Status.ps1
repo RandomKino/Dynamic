b3e3de8489735307c53c60e1b9bb3305bbed87df
Kino <61402104+RandomKino@users.noreply.github.com>
2023-02-27 11:43:50 -0500

    Update Get-Activation-Status.ps1

--- functions/Get-Activation-Status.ps1
+++ functions/Get-Activation-Status.ps1
@@ -1,5 +1,5 @@
 # Return Windows Activation status
-# Return boolean, true = activated, false = not activated.
+# Return boolean, true = activated, false = not activated
 
 function Get-Activation-Status {
     $ACTRESULT = (cscript C:\Windows\System32\slmgr.vbs /xpr | Select-Object -index 4).ToString().Trim()
