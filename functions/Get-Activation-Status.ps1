9e47ab58292ac279c2d863c65453178d4e8516cf
Kino <61402104+RandomKino@users.noreply.github.com>
2023-02-27 11:31:41 -0500

    Create Get-Activation-Status.ps1

---
+++ functions/Get-Activation-Status.ps1
@@ -0,0 +1,11 @@
+# Return boolean, true = activated, false = not activated.
+
+function Get-Activation-Status {
+    $ACTRESULT = (cscript C:\Windows\System32\slmgr.vbs /xpr | Select-Object -index 4).ToString().Trim()
+
+    if ($ACTRESULT -eq "The machine is permanently activated.") {
+        Return $true
+    } else {
+        Return $false
+    }
+}
