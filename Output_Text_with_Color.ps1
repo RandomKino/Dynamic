18556c01b2d63aed4febcc6ed0608e2bb1ba56ac
Kino <61402104+RandomKino@users.noreply.github.com>
2023-03-16 14:22:30 -0400

    Add files via upload

---
+++ Output_Text_with_Color.ps1
@@ -0,0 +1,16 @@
+﻿function OutputText($Text,$Color){
+    $j = $Text.Length
+    for ($i = 1; $i -le $j; $i++) {
+       $Lines = $Lines + "-"
+    }
+    Write-Host "`n"
+    Write-Host $Lines
+    $Host.UI.RawUI.BackgroundColor = $Color
+    Write-Host $Text
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    Write-Host $Lines
+ }
+
+OutputText "Hello World" "Yellow"
+
+
